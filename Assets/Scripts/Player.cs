f83d111c9fd451df0386fb163c0b0304b66d03bf
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-01-05 00:19:15 +0530

    3rd person camera something

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,29 +4,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 10f;
     public float sensitivityX = 5f;
     public float sensitivityY = 5f;
     public float gravity = 9.81f;
-    public float jumpSpeed = 20f;
+    public float jumpSpeed = 30f;
+    public float smoothTime = 0.1f;
+    public float turnVelocity;
     private CharacterController characterController;
+    private Rigidbody rigidBody;
+    public Transform cam;
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        //rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float hinput = Input.GetAxis("Horizontal");
-        float vinput = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(hinput, 0, vinput)*speed;
-        direction = transform.TransformDirection(direction);
-        direction.y -= gravity;
-        if (direction.y < 0 && characterController.isGrounded) direction.y = 0f;
-        characterController.Move(direction*Time.deltaTime);
-        float rotateX = Input.GetAxis("Mouse X") * sensitivityX;
-        transform.localEulerAngles += new Vector3(0, rotateX, 0);
+        float hinput = Input.GetAxisRaw("Horizontal");
+        float vinput = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(hinput, 0, vinput).normalized;
+        if(direction.magnitude>=0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, smoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            direction = (Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward).normalized*speed;
+            if (Input.GetKey(KeyCode.LeftShift)) direction *= 2;
+            characterController.Move(direction * Time.deltaTime);
+        }
+        
+        
     }
 }
